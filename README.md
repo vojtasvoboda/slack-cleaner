fda233cc767a44de13d0a74ae600abcdf0fb4c23
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-01-13 13:55:04 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# slack-cleaner
-Clean messages from Slack
+# Slack cleaner
+
+Used for deleting direct messages from some user or bot. Now it's available only by API.
+
+# How to clean messages
+
+- find bot ID at https://api.slack.com/methods/im.list/test or 
+- find user ID at https://api.slack.com/methods/users.list/test
+- insert ID to script as `$channelName`
+- on https://api.slack.com/methods/auth.test/test click to Test Method
+- copy `token` parameter from generated URL
+- insert token to script as `$token` parameter
+- run `composer install`
+- run script
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once 'vendor/autoload.php';
+
+use Tracy\Debugger;
+Debugger::enable();
+
+/**
+ * INIT
+ * - get token from https://api.slack.com/methods/im.history/test
+ * - get channel name from https://api.slack.com/methods/im.list/test
+ */
+$channelName = '<CHANNEL-NAME>';
+$token = '<GENERATED-TOKEN>';
+$deletedCountInOneCall = 1000;
+
+// slack can get only 100 last messages in one call
+for($i = 1; $i < ($deletedCountInOneCall / 10); $i++)
+{
+    // get messages
+    $messagesCount = 100;
+    $messagesUrl = "https://slack.com/api/im.history?token=$token&channel=$channelName&count=$messagesCount&pretty=1";
+    $messagesJson = file_get_contents($messagesUrl);
+    $messages = json_decode($messagesJson);
+
+    // get message timestamps
+    $ts = [];
+    if (isset($messages->messages)) {
+        foreach($messages->messages as $m) {
+            $ts[] = $m->ts;
+        }
+    }
+
+    // delete URL
+    $deleteUrl = "https://slack.com/api/chat.delete?token=$token&channel=$channel&pretty=1&ts=";
+
+    // delete all messages
+    foreach ($ts as $t) {
+        $r = file_get_contents($deleteUrl . $t);
+    }
+
+    echo "deleted" . sizeof($ts) . " messages" . "<br />";
+}
+
+exit("end");
